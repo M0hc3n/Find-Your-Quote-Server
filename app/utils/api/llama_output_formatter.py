a2c745a1d8efce7b25e30b72f5af7ad3052ab436
Mohcen Taher Chouireb <mouhcen.chouireb@ensia.edu.dz>
2024-11-09 15:24:11 +0100

    fixed broken json problems

--- app/utils/api/llama_output_formatter.py
+++ app/utils/api/llama_output_formatter.py
@@ -1,8 +1,8 @@
 from utils.api.api_models import SimilarQuotesResponse
-import json
+import json_repair
 
 def output_formatter(output: str):
-    json_output = json.loads(output)
+    json_output = json_repair.loads(output)
     
     res = {
         "response": json_output['quote'],
--- app/utils/model/call_llama.py
+++ app/utils/model/call_llama.py
@@ -4,7 +4,7 @@ from utils.core.config import llama_config
 
 def call_llama(prompt: str):
     
-    input = {**llama_config, 'prompt': prompt}
+    input = {**llama_config, 'prompt': f"I want you to find a quote which best express this feeling or this half-remembered quote: {prompt}. I want you to give me the quote, its author, and in what book he said it, in a JSON style. Return Only the JSON output, with no extra information"}
     
     output = replicate.run(
         "meta/meta-llama-3-8b-instruct",
